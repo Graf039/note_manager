657538c72736c1d3a0992a5094f3e96cfe3a6473
Graf039 <djbarongraf@gmail.com>
2025-01-21 16:58:14 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+# 1
+username = "graf"
+title = "holiday"
+content = "day_off"
+status = "activ"
+created_date = "31-01-2025"
+issue_date = "28-02-2025"
+# Очень интересно)
+print("graf:", username)
+print("holiday:", title)
+print("day_off:", content)
+print("activ:", status)
+print("31-01-2025:", created_date)
+print("28-02-2025:", issue_date)
