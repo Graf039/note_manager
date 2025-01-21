571473da29c0f60a956bc8dfc289a2dec82fd89e
Graf039 <djbarongraf@gmail.com>
2025-01-21 18:17:08 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,7 @@
+created_date = "31-01-2025"
+issue_date = "28-02-2025"
+temp_issue_date = created_date [0:5]
+temp_created_date = issue_date [:-5]
+
+print(temp_issue_date)
+print(temp_created_date)
