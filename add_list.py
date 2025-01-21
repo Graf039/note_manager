47e4ec85f822243e48d88791979128b42f255a2b
Graf039 <djbarongraf@gmail.com>
2025-01-22 01:13:39 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,8 @@
+title1 = input("Акт 1 (Придумайте название): ")
+title2 = input("Акт 2 (Придумайте название): ")
+title3 = input("Акт 3 (Придумайте название): ")
+titles = [title1, title2, title3]
+print(title1)
+print(title2)
+print(title3)
+print("Название актов:", titles)
